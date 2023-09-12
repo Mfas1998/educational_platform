7fe9fba12e281b726d3a5f988b857774f23a3aa1
ahmed hayeal <bakilalsabry6@gmail.com>
2023-09-12 16:58:12 -0700

    modifide card width

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -7,7 +7,7 @@ function Card(props) {
             <div className=' w-44 flex max-[700px]:w-28  max-[700px]:pr-2  items-center justify-center pr-14 py-2 border rounded-2xl bg-white'>
                  {/* <img src={require("./images.jfif" )}  alt="" className=' w-16 h-12'/> */}
                 <div className='pl-1 text-start'>
-                    <h4 className='font-bold'>3000</h4>
+                    <h4 className='font-bold'>3010</h4>
                     <p className=' text-gray-500'>{props.description}</p>
                 </div>
                 <div className={props.className}>{props.icon}</div>
