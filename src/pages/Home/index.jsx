b8ad82230d5a8bae3fb0443363a80510b95579b9
Mfas1998 <mfas7381@gmail.com>
2023-09-15 00:29:45 +0300

    set the footer always at the bottom of the page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,11 +5,11 @@ import Footer from "../../layout/Footer"
 const Home = () => {
  
     return(
-        <>
+        <div className="flex flex-col min-h-screen">
         <Header/>
         <div>home</div>
         <Footer />
-        </>
+        </div>
     )
    
   
