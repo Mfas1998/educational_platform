7daa3f1fce94ee97f3b0d0a2b2e090ffa8b872b8
ahmed hayeal <bakilalsabry6@gmail.com>
2023-09-14 13:19:26 -0700

    Final edit

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -44,14 +44,14 @@ const Footer = () => {
           }
         />
       </div>
-      <div className="bg-primary px-20 pt-16 pb-4 text-textColor -mt-56 w-full">
+      <div className="bg-primary px-20 pt-16 pb-4 text-gray-300 text-sm -mt-56 w-full">
 
         <div className="grid grid-cols-4 max-[700px]:grid-cols-2 place-items-center">
 
           <div className="" >
             <div className=" flex  gap-4 items-center justify-start">
               <img
-                src={require("../../assets/icons/logo.png")}
+                //src={require("../../assets/icons/logo.png")}
                 alt=""
                 className=" w-16 h-10 rounded-full"
               />
@@ -59,7 +59,7 @@ const Footer = () => {
                 مكــــاوش
               </h1>
             </div>
-            <div className="text-sm mt-2 font-extralight">
+            <div className="text-xs pt-8 font-extralight">
               <p>
                 You’re not connected And the web just isn’t the same without
                 you. Let’s get you back online Check that your network cables
@@ -68,10 +68,10 @@ const Footer = () => {
               </p>
             </div>
           </div>
-          <div className="text-start pl-8 py-6">
-            <h1 className="font-bold font-serif text-white">الروابط السريعة</h1>
+          <div className="text-start pt-6 font-bold place-items-center justify-center">
+            <h1 className="font-bold text-lg font-serif text-white">الروابط السريعة</h1>
             <div className="py-8">
-            <div className="grid grid-cols-2 gap-x-5 place-items-center justify-center">
+            <div className="grid grid-cols-2 gap-x-7 gap-y-5 ">
               <p>
                 <Link to="#">عنا</Link>
               </p>
@@ -87,21 +87,21 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className=" text-start pl-8 py-6">
-            <h1 className="font-bold font-serif  text-white">أتصل بنا</h1>
-            <div className="py-8">
-              <div className="flex gap-3 items-center">
+          <div className=" text-start pt-6  place-items-center justify-center">
+            <h1 className="font-bold font-serif text-lg text-white">أتصل بنا</h1>
+            <div className="grid py-8  gap-y-5">
+              <div className="flex gap-3 ">
                 <p>
-                  {" "}
+                 
                   <IoMdCall />
                 </p>
                 <p>
                   <Link to="#">739-158-148</Link>
                 </p>
               </div>
-              <div className="flex gap-3 items-center">
+              <div className="flex gap-3 ">
                 <p>
-                  {" "}
+                
                   <AiOutlineMail />
                 </p>
                 <p>
@@ -110,9 +110,9 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="flex gap-3 justify-center items-center">
-            <p>
-              <CiLocationOn />
+          <div className="flex gap-3 justify-center items-center pt-2">
+            <p> 
+              <CiLocationOn /> 
             </p>
             <p>
               <Link to="#">40 Yem dr.Taiz gmall street</Link>
@@ -125,7 +125,7 @@ const Footer = () => {
           <div className=" text-start w-auto">
             <span>Copyright 2023 | All Rights Reserved</span>
           </div>
-          <div className="flex justify-between w-40 pr-16 text-start mr-96">
+          <div className="flex justify-between text-start gap-7">
             <Link to="#" className=" block ">
               <CiFacebook />
             </Link>
