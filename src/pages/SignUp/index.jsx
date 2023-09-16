c85b2ada4199bf1f604925e7dd9f5712faa3d2c9
Mfas1998 <mfas7381@gmail.com>
2023-09-17 02:46:28 +0300

    add styles

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -4,6 +4,7 @@ import { Checkbox, Form } from "antd";
 import ButtonComponent from "../../components/Button";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebookF } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
@@ -30,7 +31,7 @@ const SignUp = () => {
             </Form.Item>
             <Form.Item name="password">
               <InputPasswordComponent
-               required
+                required
                 placeholder="كلمة المرور"
                 id="password"
                 className=""
@@ -38,38 +39,45 @@ const SignUp = () => {
             </Form.Item>
             <Form.Item name="passwordConfirmation">
               <InputPasswordComponent
-               required
+                required
                 placeholder="تأكيد كلمة المرور"
                 id="passwordConfirmation"
                 className=""
               />
             </Form.Item>
           </div>
-          <div className="flex gap-3 items-center justify-center">
-            <Form.Item className="m-0">
-              <Checkbox className="" />
-            </Form.Item>
-            <p>I accept the terms & Conditions</p>
+          <div className="flex gap-3 items-center justify-between">
+            <div className="flex gap-3 items-center">
+              <Form.Item className="m-0">
+                <Checkbox className="" />
+              </Form.Item>
+              <p>اوافق على الاحكام والشروط</p>
+            </div>
+
             <ButtonComponent
               htmlType="submit"
               title="SIGN UP"
               className="bg-primary text-textColorprimary rounded-full"
             />
           </div>
-            <p className="text-xl my-3 w-full text-center  before:inline-block before:w-[46%] before:h-0.5 before:bg-primary  after:inline-block after:w-[46%] after:h-0.5 after:bg-primary ">
-              or
-              </p>
-          <ButtonComponent
-            title="تسجيل بإستخدام جوجل"
-            icon={<FcGoogle />}
-            className="rounded-full w-full"
-          />
-          <ButtonComponent
-            title="تسجيل بإستخدام فيسبوك"
-            icon={<FaFacebookF className="text-blue-700" />}
-            className="rounded-full w-full mt-3"
-          />
         </Form>
+
+        <p className="text-xl my-5 w-full text-center  before:inline-block before:ml-3 before:w-[40%] before:h-0.5 before:bg-primary  after:inline-block after:mr-3 after:w-[40%] after:h-0.5 after:bg-primary ">
+          or
+        </p>
+        <ButtonComponent
+          title="تسجيل بإستخدام جوجل"
+          icon={<FcGoogle />}
+          className="rounded-full w-full"
+        />
+        <ButtonComponent
+          title="تسجيل بإستخدام فيسبوك"
+          icon={<FaFacebookF className="text-blue-700" />}
+          className="rounded-full w-full mt-3"
+        />
+        <p className="text-center mt-8 ">
+         لديك حساب بالفعل ؟ <Link to="/Login" className="font-bold">تسجيل الدخول</Link>
+        </p>
       </div>
     </div>
   );
