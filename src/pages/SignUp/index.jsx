6227c72b4947c82f73753c930c438b064b9f8eb7
Mfas1998 <mfas7381@gmail.com>
2023-09-17 00:10:57 +0300

    add some styles

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,70 +1,77 @@
 import React from "react";
 import InputComponent, { InputPasswordComponent } from "../../components/input";
-import { Checkbox, Form,  } from "antd";
+import { Checkbox, Form } from "antd";
 import ButtonComponent from "../../components/Button";
-import { FcGoogle } from 'react-icons/fc';
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebookF } from "react-icons/fa";
 
 const SignUp = () => {
   return (
     <div className=" flex h-[100vh] items-center justify-center ">
-
       <div className="p-2">
-       <Form>
-        <div>
-          <p className="text-xl">إنشاء حساب</p>
+        <Form>
+          <div>
+            <p className="text-xl">إنشاء حساب</p>
             <Form.Item name="userName">
               <InputComponent
-              required
+                required
                 placeholder="إسم المستخدم"
                 id="user_name"
-                className=" focus:border-primary text-center"
+                className=""
               />
             </Form.Item>
             <Form.Item name="email">
               <InputComponent
-               required
+                required
                 placeholder="الايميل"
                 id="email"
-                className=" focus:border-primary text-center"
+                className=""
               />
             </Form.Item>
             <Form.Item name="password">
               <InputPasswordComponent
                 placeholder="كلمة المرور"
                 id="password"
-                className=" focus:border-primary placeholder:!text-center text-center"
+                className=""
               />
             </Form.Item>
             <Form.Item name="passwordConfirmation">
               <InputPasswordComponent
                 placeholder="تأكيد كلمة المرور"
                 id="passwordConfirmation"
-                className=" focus:border-primary !text-center"
+                className=""
               />
             </Form.Item>
-        </div>
-        <div className="flex gap-3">
-        <ButtonComponent
-          htmlType='submit'
-            title="SIGN UP"
-            className="bg-primary text-textColorprimary !rounded-full"
+          </div>
+          <div className="flex gap-3 items-center justify-center">
+            <Form.Item className="m-0">
+              <Checkbox />
+            </Form.Item>
+            <p>I accept the terms & Conditions</p>
+            <ButtonComponent
+              htmlType="submit"
+              title="SIGN UP"
+              className="bg-primary text-textColorprimary rounded-full"
+            />
+          </div>
+
+          <div className="text-xl flex gap-2 items-center justify-center">
+            <p className="w-full h-0.5 bg-primary"></p>
+            <p className="text-center">or</p>
+            <p className="w-full h-0.5 bg-primary"></p>
+          </div>
+          <ButtonComponent
+            title="Sign Up with Google"
+            icon={<FcGoogle />}
+            className="rounded-full w-full"
           />
-          <Form.Item 
-          className="m-0"
-          label="I accept the terms & Conditions">
-            <Checkbox/>
-          </Form.Item>
-        </div>
-
-        <div className="text-xl flex gap-2 items-center justify-center">
-          <p className="w-full h-0.5 bg-primary"></p>
-          <p className="text-center">or</p>
-          <p className="w-full h-0.5 bg-primary"></p>
-        </div>
-       <ButtonComponent title='Sign Up with Google' icon={<FcGoogle/>} className='rounded-full w-full' />
-      </Form>
+          <ButtonComponent
+            title="Sign Up with Facebook"
+            icon={<FaFacebookF className="text-blue-700" />}
+            className="rounded-full w-full mt-3"
+          />
+        </Form>
       </div>
-
     </div>
   );
 };
