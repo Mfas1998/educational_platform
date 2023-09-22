b89aac81c0215c4900bc2e1844d4342374f752bf
Mohammed <mfas7381@gmail.com>
2023-09-22 21:29:37 +0300

    ..

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-
+import { AiFillPlayCircle } from "react-icons/ai";
+import img from "../../assets/images/lerning.jpg";
+import { Rate } from "antd";
 function Card(props) {
   return (
     <div className={props.styles}>
@@ -12,6 +13,43 @@ function Card(props) {
       </div>
 
       <p className={props.styleText}>{props.text}</p>
+    </div>
+  );
+}
+
+export function CourseCard(props) {
+  return (
+    <div
+      className="rounded-2xl h-[250px] min-w-[260px] mt-4 bg-white"
+    >
+      <div>
+        <img src={img} alt="" className="h-[100px] w-full rounded-t-2xl" />
+      </div>
+      <div className="p-3 h-[150px] ">
+        <div className="flex items-center justify-between text-[10px]">
+          <Rate allowHalf defaultValue={2.5} className="text-[10px]" />
+          <div className="flex items-center gap-2  ">
+            <p>25x Lesson</p>
+            <AiFillPlayCircle className="text-green-500 text-[18px]" />
+          </div>
+        </div>
+
+        <p className=" text-sm text-left my-2">
+          Powerful mental tools to help you master tough subject
+        </p>
+        <hr/>
+        <div className="flex items-center justify-between mt-2 h-max text-[10px] ">
+            <p className="bg-primary rounded-full px-4 text-textColorprimary">Design</p>
+
+          <div className="flex items-center gap-2 ">
+           <div>
+              <p className="font-bold">Wade Warren</p>
+              <p>Web Developer</p>
+           </div>
+            <img src={img} alt="" className="h-7 w-7 rounded-full"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
